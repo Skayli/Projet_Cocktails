1725fd6d7c84a24b8c10c48e90bbc6cd8dc744c5
Quentin <quentin.nade27@gmail.com>
2017-10-28 11:29:00 +0200

    Quelques modifications mineurs & ajouts de commentaires (surtout dans index.php)

--- index.php
+++ index.php
@@ -15,7 +15,8 @@ include_once 'Includes/Donnees.inc.php';
 		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu" id="get-aliment">';
 	
 	//Boucle qui va créer les premier éléments 
-	//et appeler la fonction diplay() pour les sous-catégories
+	//et appeler la fonction diplay() pour les sous-catégories (cf: https://arche.univ-lorraine.fr/pluginfile.php/486869/mod_resource/content/6/ProjetPhpRecettesCuisine.WEB2.2017.pdf )
+	//!! Cette partie peut surement être améliorée (modifier la fonction diplay() afin de n'appeler qu'elle dans la boucle par exemple)
 	foreach($Hierarchie["Aliment"]["sous-categorie"] as $topCateg)
 	{
 		if(array_key_exists("sous-categorie",$Hierarchie[$topCateg]))
@@ -67,7 +68,6 @@ include_once 'Includes/Donnees.inc.php';
 		}
 	}
 	//FIN display()
-	
 	$superCateg = $superCateg.'</ul>';
 	asort($ingredients);
 	
@@ -79,9 +79,11 @@ include_once 'Includes/Donnees.inc.php';
 <html>
 
 <head>
+<!-- Recupération des bibliotheques JS et jquery en ligne --> 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 	
+<!-- Fichier créés -->	
     <link href="resources/css/bootstrap.css" rel="stylesheet">
 	<link href="resources/css/style.css" rel="stylesheet">
 		
@@ -91,60 +93,81 @@ include_once 'Includes/Donnees.inc.php';
 </head>
 
 <body>
-	<div class="wrap">
-	<?php echo $header;?>
 	
-	<div class="container-fluid" id="navbar">
-		<div class="container">
-			<ul class="nav nav-pills" id="test">
-				<li role="presentation" class="active">
-					<a href="#" class="text-nav-bar">Accueil</a>
-				</li>
-				<li role="presentation" class="dropdown">
-					<a href="#" class="dropdown-toggle text-nav-bar" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Aliments
-						<span class="caret"></span>
-					</a> 
-					<input type="hidden" id="search-aliment" name="search-aliment" value="nothing">
-					<?php echo $superCateg ?>
-				</li>
-				<li role="presentation">
-					<a href="#" class="text-nav-bar">Recettes</a>
-				</li>
-				<li role="presentation">
-					<a href="#" class="text-nav-bar">Inscription/Connexion</a>
-				</li>
-
-				<div class="col-sm-5 col-md-5 pull-right">
-					<form class="navbar-form" role="search">
-						<div class="input-group">
-							<input list="browsers" class="form-control" placeholder="Rechercher">
-							<datalist id="browsers">
-							  <?php foreach($ingredients as $allIngredients => $nom)
-							  {
-								  echo '<option value="'.$nom.'" />';
-							  } ?>
-							</datalist>
-							<div class="input-group-btn">
-								<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-							</div>
-						</div>
-					</form>
-				</div>
+	<!-- Div d'affichage : Contient toute la page sauf le footer -->
+	<div class="wrap"> 
+		<?php echo $header;?>
+		
+		<!-- BARRE DE NAVIGATION -->
+		<div class="container-fluid" id="navbar">
+			<div class="container">
 			
-			</ul>
+				<ul class="nav nav-pills" id="test"><!-- La navbar contient une LISTE de tous les "boutons" | LI == BOUTON -->
+				
+					<li role="presentation" class="active"><!-- class="active" => bouton surligné -->
+						<a href="#" class="text-nav-bar">Accueil</a><!-- Bouton vers la page d'accueil -->
+					</li>
+					
+					<li role="presentation" class="dropdown"><!-- Bouton multi-liste avec tous les aliments -->
+						<a href="#" class="dropdown-toggle text-nav-bar" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Aliments
+							<span class="caret"></span>
+						</a> 
+						<input type="hidden" id="search-aliment" name="search-aliment" value="nothing"><!-- Input dans lequel on récupère la valeur de l'aliment "feuille" cliqué (cf "script.js") -->
+						<?php echo $superCateg ?>
+					</li>
+					
+					<li role="presentation"><!-- Bouton qui affichera la page ou seront présentées toutes les recettes -->
+						<a href="#" class="text-nav-bar">Recettes</a>
+					</li>
+					
+					<li role="presentation"><!-- Bouton qui affichera la page des formulaire de connexion/inscription (à convenir : comment sauvegarder les données d'un utilisateur) -->
+						<a href="#" class="text-nav-bar">Inscription/Connexion</a>
+					</li>
+					
+					<!-- Zone de saisie à droite, contenant une datalist -->
+					<div class="col-sm-5 col-md-5 pull-right">
+						<form class="navbar-form" role="search">
+							<div class="input-group">
+								<input list="browsers" class="form-control" placeholder="Rechercher">
+								<datalist id="browsers">
+								  <?php foreach($ingredients as $allIngredients => $nom)
+								  {
+									  echo '<option value="'.$nom.'" />';
+								  } ?>
+								</datalist>
+								<div class="input-group-btn"><!-- Bouton avec la loupe pour lancer la recherche -->
+									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+								</div>
+							</div>
+						</form>
+					</div>
+				
+				</ul>
+				
+			</div>
 		</div>
-	</div>
-	
-	
-
+		
+		<!-- //BARRE DE NAVIGATION -->
+		
+		<!-- CONTENU PRINCIPAL -->
+		<!--
+		L'idée est de séparer les différents contenus dans différentes pages (au format PHP) 
+		et d'include la bonne quand nécessaire (selon le bouton de la navbar cliqué, on affiche accueil.php, login.php, etc ... 
+		-->
 		<div class="container main-content">
-		
-			<?php include 'accueil.php' ?>
-		
+			
+				<?php include 'accueil.php' ?>
+			
 		</div>
+		<!-- //CONTENU PRINCIPAL -->
+		
+		<!-- Div d'affichage : pousse le footer pour qu'il ne soit pas au dessus du texte de bas de page -->
 		<div class="push"></div>
-	</div>
 
+	
+	</div>
+	<!-- //WRAP -->
+	
 	<?php echo $footer; ?>
 	
  </body>
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,25 +1,16 @@
 $(document).ready(function() {
 	
+	//Mise en forme des balise pre & code (sera surement inutile plus tard)
 	$("pre code").each(function(){
 		var html = $(this).html();
 		var pattern = html.match(/\s*\n[\t\s]*/);
 		$(this).html(html.replace(new RegExp(pattern, "g"),'\n'));
 	});
 
-	//Permet d'avoir un aliment
+	//Permet d'avoir un aliment (via l'input HIDDEN de la navbar)
 	$("#get-aliment #li-sousCategorie").click(function(){
 		$("#search-aliment").val($(this).attr("value"));
 		alert("Vous avez choisi : " + $("#search-aliment").val());
-	});
-		
-		
-		
-	$('.li-submenu').mousedown(function(e) {
-		
-	});
-
-	$('.li-submenu').click(function(e) {
-		
 	});
 
 });
